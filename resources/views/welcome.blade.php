0d1f3ab8ae0ffd83882a093fc6fdc4dfd8e64a02
ryj <jerryrenzly@foxmail.com>
2020-11-27 17:34:04 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,6 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
                 <div class="links">
                     <a href="https://laravel.com/docs">Docss</a>
                     <a href="https://laracasts.com">Laracasts</a>
