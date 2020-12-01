44fea1629fbdac6078f9e2ab186ac39ecda6cc62
ryj <jerryrenzly@foxmail.com>
2020-12-01 11:38:13 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <img class="brand-icon" src="https://cdn.learnku.com//uploads/communities/WtC3cPLHzMbKRSZnagU9.png!/both/44x44">
+<a href="https://laravel-china.org/courses" target=_blank>
+    刻意练习，每日精进
+    </a>
+    <div class="float-right">
+        <a href="/about" >关于</a>
+    </div>
+</footer>
